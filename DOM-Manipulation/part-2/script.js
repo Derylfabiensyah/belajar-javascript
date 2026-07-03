b7812f054b2bdd840284cbbaec9f6b5383b8ddfb
Derylfabiensyah <derylfabien09@gmail.com>
2026-07-03 21:24:34 +0700

    DOM Manipulation part 2

--- DOM-Manipulation/part-2/script.js
+++ DOM-Manipulation/part-2/script.js
@@ -15,4 +15,27 @@ const li = document.createElement('li');
 const teksLiBaru = document.createTextNode('Item Baru');
 li.appendChild(teksLiBaru);
 
-const ul = document.que
+const ul = document.querySelector('section#b ul');
+const li2 = ul.querySelector('section#b li:nth-child(2)');
+
+ul.insertBefore(li, li2);
+
+
+const link = document.getElementsByTagName('a')[0];
+
+sectionA.removeChild(link);
+
+const sectionB = document.getElementById('b');
+const p4 = sectionB.querySelector('p');
+
+const h2Baru = document.createElement('h2');
+const h2TeksBaru = document.createTextNode('Judul Baru!');
+
+h2Baru.appendChild(h2TeksBaru);
+
+sectionB.replaceChild(h2Baru, p4); 
+
+
+pBaru.style.backgroundColor = 'salmon';
+h2Baru.style.backgroundColor = 'salmon';
+li.style.backgroundColor = 'salmon';
