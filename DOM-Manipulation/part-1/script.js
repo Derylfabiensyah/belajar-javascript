33ca980c88437ea75f5a0e12429ebcb22217e409
Derylfabiensyah <derylfabien09@gmail.com>
2026-07-03 20:07:24 +0700

    DOM Manipulation part 1

---
+++ DOM-Manipulation/part-1/script.js
@@ -0,0 +1,48 @@
+// element.innerHTML
+// const judul = document.getElementById('judul');
+// judul.innerHTML = 'deryl'
+
+// element.style.<propertycss>
+// const judul = document.querySelector('#judul');
+// judul.style.color = 'salmon';
+// judul.style.backgroundColor = 'purple';
+
+// element.setAttribute()
+// const judul = document.getElementsByTagName('h1')[0];
+// judul.setAttribute('name', 'deryl');
+
+// const a = document.querySelector('section#a a');
+// a.setAttribute('id', 'link');
+
+// element.getAttribute()
+// console.log(a.getAttribute('href'));
+
+// element.removeAttribute()
+// a.removeAttribute('id');
+
+// element.classList
+// element.classList.add()
+// const p2 = document.querySelector('.p2');
+// p2.classList.add('label')
+
+// element.classList.remove()
+// p2.classList.remove('label');
+
+// element.classList.toggle()
+// p2.classList.toggle('label');
+
+// const body = document.body;
+// body.classList.toggle('biru-muda');
+// p2.classList.add('satu');
+// p2.classList.add('dua');
+// p2.classList.add('tiga');
+
+// element.classList.item()
+// console.log(p2.classList.item(3));
+
+// element.classList.contains()
+// console.log(p2.classList.contains('dua'));
+// console.log(p2.classList.contains('label'));
+
+// element.classList.replace()
+// p2.classList.replace('tiga', 'label');
