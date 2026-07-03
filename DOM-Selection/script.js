ade48cdeef09d20a03105d19fc41363055e06a3e
Derylfabiensyah <derylfabien09@gmail.com>
2026-07-03 19:14:37 +0700

    file belajar DOM Selection

---
+++ DOM-Selection/script.js
@@ -0,0 +1,34 @@
+// DOM SELECTION
+// document.getElementById()
+// const judul = document.getElementById('judul');
+// judul.style.color = 'pink';
+
+// document.getElementsByTagName()
+// const p1 = document.getElementsByTagName('p')[0];
+// p1.style.backgroundColor = 'salmon';
+
+// document.getElementsByTagName()
+// const p = document.getElementsByTagName('p');
+// p[0].style.backgroundColor = 'salmon';
+// p[1].style.backgroundColor = 'salmon';
+// p[2].style.backgroundColor = 'salmon';
+// p[3].style.backgroundColor = 'salmon';
+
+// document.getElementsByClassName()
+// const p1 = document.getElementsByClassName('p1')[0];
+// p1.innerHTML = 'ini diubah dari javascript';
+// p1.style.backgroundColor = 'salmon';
+        
+// document.querySelector()
+// const p4 = document.querySelector('#b p');
+// p4.style.color = 'pink';
+// p4.style.fontSize = '30px';
+
+// const li2 = document.querySelector('section#b ul li:nth-child(2)');
+// li2.style.backgroundColor = 'salmon';
+
+// document.querySelectorAll()
+// const p = document.querySelectorAll('p');
+// for(let i = 0; i < p.length; i++){
+//     p[i].style.backgroundColor = 'brown';
+// }
